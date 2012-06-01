73b18c1dffe5507a72e9d818d288fd4e5b1d2a54
Paul <schwendenman.paul@gmail.com>
2012-05-31 23:00:37 -0400

    Removed will now use two spaces

--- client.lisp
+++ client.lisp
@@ -1,69 +1,43 @@
-;(defmacro iff (b a c question then colon else) `(if (,a ,b ,c) ,then ,else))
-
-(defun repl ()
-	(setf reply (read server))
-	(format t "~A~%" reply)
-	(setf command (read1))
-	(print command server)
-	(unless (eq (car command) 'quit)
-		(setf reply (read server))
-		(print (read server))
-		(repl)))
-;(defun repl ()
-;	(setf reply (read server))
-;	(if (eq `,reply 'done)
-;		(print (read server))
-;		(format t "~A~%" (read server)))
-;	(setf command (read1))
-;	(print command server)
-;	(unless (eq (car command) 'quit)
-;		(setf reply (read server))
-;			(print (read server))
-;			(progn
-;				(format t "~A~%" reply)
-;				(repl)))))
-
-
 (defun read1 ()
-	(format t "~%> ")
-	(read-from-string (concatenate 'string "(" (read-line)  ")")))
+  (format t "~%> ")
+  (read-from-string (concatenate 'string "(" (read-line)  ")")))
 
 (progn
-	; Get Name For Session
-	(format t "Name: ")
-	(setf name (read))
-	
-	(loop
-		;Make connection
-		(defparameter server (socket-connect 4321 "127.0.0.1"))
-		
-		;Send Name
-		(print name server)
-		(format t "Connected~%")
-		(format t "~A~%" (read server))
-		
-		; Print Cards
-		(format t "~A~%" (read server))
-		
-		; Get Command
-		(loop
-			(setf command (read1))
-			(print command server)
-			(if (eq (car command) 'ragequit) (return))
-			(if (eq (car command) 'close) (return))
-		
-			; Get Response
-			(setf response (read server))
-			(if (not (eq response 'done))
-			(format t "~A~%" response)
-			(return)))
+  ; Get Name For Session
+  (format t "Name: ")
+  (setf name (read))
+  
+  (loop
+    ;Make connection
+    (defparameter server (socket-connect 4321 "127.0.0.1"))
+    
+    ;Send Name
+    (print name server)
+    (format t "Connected~%")
+    (format t "~A~%" (read server))
+    
+    ; Print Cards
+    (format t "~A~%" (read server))
+    
+    ; Get Command
+    (loop
+      (setf command (read1))
+      (print command server)
+      (if (eq (car command) 'ragequit) (return))
+      (if (eq (car command) 'close) (return))
+    
+      ; Get Response
+      (setf response (read server))
+      (if (not (eq response 'done))
+      (format t "~A~%" response)
+      (return)))
 
-		(if (eq (car command) 'quit) (return))
-		(if (eq (car command) 'close) (return))
+    (if (eq (car command) 'quit) (return))
+    (if (eq (car command) 'close) (return))
 
-		; Get Result		
-		(format t "~A~%" (read server))
-		
-		; Close Connection
-		(format t "~%~%............~%~%")
-		(close server)))
+    ; Get Result    
+    (format t "~A~%" (read server))
+    
+    ; Close Connection
+    (format t "~%~%............~%~%")
+    (close server)))
--- server.lisp
+++ server.lisp
@@ -4,27 +4,27 @@
 
 ; p is the card defstruct here
 (defun print-card (p stream depth)
-	(format stream "~A of ~A," (cdr (assoc (card-rank p) rank)) (cdr (assoc (card-suit p) suit))))
+  (format stream "~A of ~A," (cdr (assoc (card-rank p) rank)) (cdr (assoc (card-suit p) suit))))
 
 (defstruct (card (:print-function print-card))
-	suit
-	rank)
-	
+  suit
+  rank)
+  
 (defun get-value (card)
-	(case (card-rank card)
-		(1 11)
-		(11 10)
-		(12 10)
-		(13 10)
-		(otherwise (card-rank card))))
+  (case (card-rank card)
+    (1 11)
+    (11 10)
+    (12 10)
+    (13 10)
+    (otherwise (card-rank card))))
 
 ; Set up the suits for 'pretty' printing of cards
 (setf suit '((1 . "Spades")(2 . "Hearts")(3 . "Spades")(4 . "Diamonds")))
 
 ; Set up the ranks for 'pretty' printing of cards
 (setf rank '((1 . "Ace")(2 . "2")(3 . "3")(4 . "4")
-	(5 . "5")(6 . "6")(7 . "7")(8 . "8")(9 . "9")
-	(10 . "10")(11 . "Jack")(12 . "Queen")(13 . "King")))
+  (5 . "5")(6 . "6")(7 . "7")(8 . "8")(9 . "9")
+  (10 . "10")(11 . "Jack")(12 . "Queen")(13 . "King")))
 
 
 ; * * * * * * * * * * * * * * * * * * * * *
@@ -32,43 +32,43 @@
 ; * * * * * * * * * * * * * * * * * * * * *
 
 (defun print-player (p stream depth)
-	(format stream "~A: ~A" (player-name p) (player-cards p)))
+  (format stream "~A: ~A" (player-name p) (player-cards p)))
 
 ; prints all but the dealer's first card
 (defun print-dealer (dealer stream)
     (format stream "Dealer: *** of ***, ~A" (cdr (player-cards dealer))))
 
 (defstruct (player (:print-function print-player))
-	name
-	cards)
-	
+  name
+  cards)
+  
 (defun new-player (name)
-	(make-player :name name :cards ()))
-	
+  (make-player :name name :cards ()))
+  
 (defun add-card (player card)
-	(if (null card)
-	nil
-	(setf (player-cards player) (cons card (player-cards player)))))
-	
+  (if (null card)
+  nil
+  (setf (player-cards player) (cons card (player-cards player)))))
+  
 
 ; * * * * * * * * * * * * * * * * * * * * *
 ; * Hash Table				  *
 ; * * * * * * * * * * * * * * * * * * * * *
 
 (defun print-player-state (p stream depth)
-	(format stream "~A of ~A games played."  (player-state-wins p) (player-state-games-played p)))
+  (format stream "~A of ~A games played."  (player-state-wins p) (player-state-games-played p)))
 
 (defstruct (player-state (:print-function print-player-state))
-	wins
-	games-played
-	password
-	bank)
-	
+  wins
+  games-played
+  password
+  bank)
+  
 (defun load-player (name)
-	(gethash name hash-table))
-	
+  (gethash name hash-table))
+  
 (defun save-player (name info)
-	(setf (gethash name hash-table) info))
+  (setf (gethash name hash-table) info))
 
 ; * * * * * * * * * * * * * * * * * * * * *
 ; * Card Helpers			  *
@@ -76,41 +76,41 @@
 
 
 (defun create-deck ()
-	(setf deck (new-player "deck"))
-	(loop for i in suit do 
-		(loop for j in rank do
-			(add-card deck (make-card :rank (car j) :suit (car i)))))
-	(player-cards deck))
+  (setf deck (new-player "deck"))
+  (loop for i in suit do 
+    (loop for j in rank do
+      (add-card deck (make-card :rank (car j) :suit (car i)))))
+  (player-cards deck))
 
 (defun create-shoe ()
-	(append (create-deck) (create-deck)  (create-deck) (create-deck) (create-deck) (create-deck)))
+  (append (create-deck) (create-deck)  (create-deck) (create-deck) (create-deck) (create-deck)))
 
 (defun shuffle (x)
-	(setf len (length x))
-	(dotimes (n len)
-		(rotatef (nth n x) (nth (random len) x)))
-	x)
+  (setf len (length x))
+  (dotimes (n len)
+    (rotatef (nth n x) (nth (random len) x)))
+  x)
 
 (defun card-sum (cards)
-	(setf sum (reduce (lambda (a b) (+ (get-value b) a)) cards :initial-value 0))
-	(if (> sum 21)
-		(correct-sum sum (length (remove-if-not (lambda (a) (= 1 (card-rank a))) cards)))
-		sum))
+  (setf sum (reduce (lambda (a b) (+ (get-value b) a)) cards :initial-value 0))
+  (if (> sum 21)
+    (correct-sum sum (length (remove-if-not (lambda (a) (= 1 (card-rank a))) cards)))
+    sum))
 
 (defun correct-sum (sum aces)
-	(if (and (> sum 21) (> aces 0))
-		(correct-sum (- sum 10) (- aces 1))
-		sum))
+  (if (and (> sum 21) (> aces 0))
+    (correct-sum (- sum 10) (- aces 1))
+    sum))
 
 ;(defun get-sum (x)
-;		(if (null x)
-;			0
-;			(+ (get-value (car x)) (get-sum (cdr x)))))
+;    (if (null x)
+;      0
+;      (+ (get-value (car x)) (get-sum (cdr x)))))
 
 (defun draw-card ()
-	(setf card (car deck))
-	(setf deck (cdr deck))
-	(values card))
+  (setf card (car deck))
+  (setf deck (cdr deck))
+  (values card))
 
 ; * * * * * * * * * * * * * * * * * * * * *
 ; * Macros				  *
@@ -129,29 +129,29 @@
 ;(setf known-variables '(nil))
 ;
 ;(defun sandboxed-eval (cmd)
-;	(if (sandbox-check cmd)
-;		(eval cmd)
-;		(format t "I do not know that command")))
+;  (if (sandbox-check cmd)
+;    (eval cmd)
+;    (format t "I do not know that command")))
 ;
 ;(defun sandbox-check (cmd)
-;	(if (null cmd)
-;	nil
-;	(or (member (car cmd) known-commands)
-;		(and (eq 'if (first cmd)) (sandbox-check-cond (second cmd)
-;		(sandbox-check (third cmd)) (sandbox-check (fourth cmd)))))))
-;		
+;  (if (null cmd)
+;  nil
+;  (or (member (car cmd) known-commands)
+;    (and (eq 'if (first cmd)) (sandbox-check-cond (second cmd)
+;    (sandbox-check (third cmd)) (sandbox-check (fourth cmd)))))))
+;    
 ;(defun sandbox-check-cond (cond)
-;	(and (member (first cond) '(> < = eq eql and or))
-;		(or (member (second cond) known-variables) (numberp (second cond)) (sandbox-check-cond (second cond)))
-;		(or (member (third cond) known-variables) (numberp (third cond)) (sandbox-check-cond (third cond)))))
+;  (and (member (first cond) '(> < = eq eql and or))
+;    (or (member (second cond) known-variables) (numberp (second cond)) (sandbox-check-cond (second cond)))
+;    (or (member (third cond) known-variables) (numberp (third cond)) (sandbox-check-cond (third cond)))))
 
 ;; With out if's
 (setf known-commands '(hit stay))
 
 (defun sandboxed-eval (cmd)
-	(if (member (car cmd) known-commands)
-		(eval cmd)
-		'(I do not know that command)))
+  (if (member (car cmd) known-commands)
+    (eval cmd)
+    '(I do not know that command)))
 
 
 ; * * * * * * * * * * * * * * * * * * * * *
@@ -159,25 +159,25 @@
 ; * * * * * * * * * * * * * * * * * * * * *
 
 (defun dealer-play ()
-	(add-card dealer (draw-card))
-	(if (< (card-sum (player-cards dealer)) 17)
-	(dealer-play)))
+  (add-card dealer (draw-card))
+  (if (< (card-sum (player-cards dealer)) 17)
+  (dealer-play)))
       
 ; * * * * * * * * * * * * * * * * * * * * *
 ; * Functions the player can run	  *
 ; * * * * * * * * * * * * * * * * * * * * *
 
 (defun hit ()
-	(add-card player (draw-card)))
+  (add-card player (draw-card)))
 
 (defun stay ()
-	(player-cards player))
+  (player-cards player))
 
 (defun again ()
-	(sandboxed-eval last-command))
+  (sandboxed-eval last-command))
 
 (defun echo ()
-	(last-command))
+  (last-command))
 
 
 ; * * * * * * * * * * * * * * * * * * * * *
@@ -186,60 +186,60 @@
 
 
 (progn
-	; Open Socket
-	(setf hash-table (make-hash-table))
-	(defparameter game-socket (socket-server 4321))
+  ; Open Socket
+  (setf hash-table (make-hash-table))
+  (defparameter game-socket (socket-server 4321))
 
-	(loop
+  (loop
 
-		; Accept Connection
-		(format t "Waiting for player...")
-		(defparameter one-stream (socket-accept game-socket))
-		(defparameter string-stream (make-string-output-stream))
-				
+    ; Accept Connection
+    (format t "Waiting for player...")
+    (defparameter one-stream (socket-accept game-socket))
+    (defparameter string-stream (make-string-output-stream))
+        
         ; seed the random number generator
         (defparameter *random-state* (make-random-state t))
-		; Make deck and Shuffle
-		(setf deck (shuffle (shuffle (create-shoe))))
-		
-		; Get Name and make player
-		(setf player-name (read one-stream))
-		(setf player (new-player player-name))
-		(setf dealer (new-player "Dealer"))
-		(format one-stream "Welcome~A!" (player-name player))
-		(format t "~A has joined~%" (player-name player))
-		
-		; Load Dealer State
-		(setf dealer-info (load-player 'dealer))
-		(if (null dealer-info) (progn (setf dealer-info (make-player-state))
-			(setf (player-state-wins dealer-info) 0)
-			(setf (player-state-games-played dealer-info) 0)))
-			
-		; Load Player State
-		(setf player-info (load-player player-name))
-		(if (null player-info) (progn (setf player-info (make-player-state))
-			(setf (player-state-wins player-info) 0)
-			(setf (player-state-games-played player-info) 0)))
-			
-		
-		; Deal Two Cards
-		(add-card player (draw-card))
-		(add-card dealer (draw-card))
-		(add-card player (draw-card))
-		(add-card dealer (draw-card))
+    ; Make deck and Shuffle
+    (setf deck (shuffle (shuffle (create-shoe))))
+    
+    ; Get Name and make player
+    (setf player-name (read one-stream))
+    (setf player (new-player player-name))
+    (setf dealer (new-player "Dealer"))
+    (format one-stream "Welcome~A!" (player-name player))
+    (format t "~A has joined~%" (player-name player))
+    
+    ; Load Dealer State
+    (setf dealer-info (load-player 'dealer))
+    (if (null dealer-info) (progn (setf dealer-info (make-player-state))
+      (setf (player-state-wins dealer-info) 0)
+      (setf (player-state-games-played dealer-info) 0)))
+      
+    ; Load Player State
+    (setf player-info (load-player player-name))
+    (if (null player-info) (progn (setf player-info (make-player-state))
+      (setf (player-state-wins player-info) 0)
+      (setf (player-state-games-played player-info) 0)))
+      
+    
+    ; Deal Two Cards
+    (add-card player (draw-card))
+    (add-card dealer (draw-card))
+    (add-card player (draw-card))
+    (add-card dealer (draw-card))
 
-		(print player string-stream)
+    (print player string-stream)
         (format string-stream "~%")     ; prints a new line
         (print-dealer dealer string-stream)
-		(print (get-output-stream-string string-stream) one-stream)
-		
+    (print (get-output-stream-string string-stream) one-stream)
+    
                 ; Get Bet <------ Maybe                                                                                                                                                      
                 ; (setf input (read one-stream))                                                                                                                                             
                 
-		(loop
-			; Hit or stay?
-			(setf input (read one-stream))
-			(if (or (> (length (player-cards player)) 4) (eq (car input) 'close) (eq (car input) 'ragequit)) (return))
+    (loop
+      ; Hit or stay?
+      (setf input (read one-stream))
+      (if (or (> (length (player-cards player)) 4) (eq (car input) 'close) (eq (car input) 'ragequit)) (return))
 
                         ; Eval 
                         (sandboxed-eval input)
@@ -251,50 +251,50 @@
                                 (progn (print player string-stream)
                                 (print (get-output-stream-string string-stream) one-stream))))
                                                                                    
-		(if (eq (car input) 'ragequit) (return))
+    (if (eq (car input) 'ragequit) (return))
                         
                         ; Save last-command
                         ;(if (not (eq (car input) 'again))
                         ;       (setf last-command input))))
-		
-		(if (not (eq (car input) 'close))
-		(progn
-		; Handle Dealer
-		(if (and (> 17 (card-sum (player-cards dealer))) (> 22 (card-sum (player-cards player))))
-			(dealer-play))
+    
+    (if (not (eq (car input) 'close))
+    (progn
+    ; Handle Dealer
+    (if (and (> 17 (card-sum (player-cards dealer))) (> 22 (card-sum (player-cards player))))
+      (dealer-play))
 
-		(print (list player dealer) string-stream)
-		
-		; Winner
-		(setf dealer-score (card-sum (player-cards dealer)))
-		(setf player-score (card-sum (player-cards player)))
-		(if (and (or (> dealer-score 21) (> player-score dealer-score) (> (length (player-cards player)) 4)) (< player-score 22))
-			(progn (print (list 'Winner player-score 'to dealer-score) string-stream)
-				(setf (player-state-wins player-info) (+ 1 (player-state-wins player-info)))
-				(setf (player-state-games-played player-info) (+ 1 (player-state-games-played player-info)))
-				(setf (player-state-games-played dealer-info) (+ 1 (player-state-games-played dealer-info))))
-			(progn (print (list 'Loser player-score 'to dealer-score) string-stream)
-				(setf (player-state-wins dealer-info) (+ 1 (player-state-wins dealer-info)))
-				(setf (player-state-games-played player-info) (+ 1 (player-state-games-played player-info)))
-				(setf (player-state-games-played dealer-info) (+ 1 (player-state-games-played dealer-info)))))
+    (print (list player dealer) string-stream)
+    
+    ; Winner
+    (setf dealer-score (card-sum (player-cards dealer)))
+    (setf player-score (card-sum (player-cards player)))
+    (if (and (or (> dealer-score 21) (> player-score dealer-score) (> (length (player-cards player)) 4)) (< player-score 22))
+      (progn (print (list 'Winner player-score 'to dealer-score) string-stream)
+        (setf (player-state-wins player-info) (+ 1 (player-state-wins player-info)))
+        (setf (player-state-games-played player-info) (+ 1 (player-state-games-played player-info)))
+        (setf (player-state-games-played dealer-info) (+ 1 (player-state-games-played dealer-info))))
+      (progn (print (list 'Loser player-score 'to dealer-score) string-stream)
+        (setf (player-state-wins dealer-info) (+ 1 (player-state-wins dealer-info)))
+        (setf (player-state-games-played player-info) (+ 1 (player-state-games-played player-info)))
+        (setf (player-state-games-played dealer-info) (+ 1 (player-state-games-played dealer-info)))))
                 (if (> (length (player-cards player)) 4) (print "Five Card Charlie" string-stream))
-		(print player-info string-stream)
-		
-		; Send Result
-		(print (get-output-stream-string string-stream) one-stream)
-		
-		; Save Player State
-		(save-player (player-name player) player-info)
+    (print player-info string-stream)
+    
+    ; Send Result
+    (print (get-output-stream-string string-stream) one-stream)
+    
+    ; Save Player State
+    (save-player (player-name player) player-info)
 
-		; Save Dealer State
-		(save-player 'dealer dealer-info)))
+    ; Save Dealer State
+    (save-player 'dealer dealer-info)))
 
-		; Close Connection
-		(close one-stream)
-		
-		(format t "~A Done ~%" (player-name player))
-		(format t "The dealer is ~A~%" (load-player 'dealer)))
-	
-	; Close Socket
-	(socket-server-close game-socket))
-	
+    ; Close Connection
+    (close one-stream)
+    
+    (format t "~A Done ~%" (player-name player))
+    (format t "The dealer is ~A~%" (load-player 'dealer)))
+  
+  ; Close Socket
+  (socket-server-close game-socket))
+  
